df5f3a9c1585a81d056ac556c89070fd55cb6ef2
Paul Yi <pauljsyi@gmail.com>
2021-01-25 21:54:02 -0800

    hidden api keys

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDmhXROw9n5EHmk6DzrNpJaO_pF3rC3DVA",
+    authDomain: "chat-app-b6f83.firebaseapp.com",
+    projectId: "chat-app-b6f83",
+    storageBucket: "chat-app-b6f83.appspot.com",
+    messagingSenderId: "666548014097",
+    appId: "1:666548014097:web:48aea623c38f3800c635b0",
+    measurementId: "G-V8SX9JQJ1M"
+}
--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import config from "./config"
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDmhXROw9n5EHmk6DzrNpJaO_pF3rC3DVA",
-  authDomain: "chat-app-b6f83.firebaseapp.com",
-  projectId: "chat-app-b6f83",
-  storageBucket: "chat-app-b6f83.appspot.com",
-  messagingSenderId: "666548014097",
-  appId: "1:666548014097:web:48aea623c38f3800c635b0",
-  measurementId: "G-V8SX9JQJ1M"
+config
 })
 
 const auth =firebase.auth();
